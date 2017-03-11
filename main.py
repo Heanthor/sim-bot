e7b88e9376815836668e9a68b99fb94f2c9968d4
Reed <reedtrevelyan@gmail.com>
2017-03-11 01:04:20 -0500

    Remove keys from code, see keys_stub.json

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import json
+
 from api.wowprogress import WarcraftLogs
 
 
 def main():
-    w = WarcraftLogs("dff497843493eac64fdcdd2eba4a9a1f")
+    with open("keys.json", 'r') as f:
+        f = json.loads(f.read())
+        warcraft_logs_public = f["warcraftlogs"]["public"]
+
+    w = WarcraftLogs(warcraft_logs_public)
     print w.get_all_parses("Heanthor", "fizzcrank", "US", "dps")
+
 
 if __name__ == '__main__':
     main()
