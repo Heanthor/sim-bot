37c56c7949c3a322c95c26abb16782dd03a530dd
Reed <reedtrevelyan@gmail.com>
2017-03-11 23:25:17 -0500

    Data structures stubs

---
+++ data_structures/warcraftlogs.py
@@ -0,0 +1,10 @@
+class ParseResponse:
+    DIFFICULTY_DICT = {
+        "lfr": 2,
+        "normal": 3,
+        "heroic": 4,
+        "mythic": 5,
+    }
+
+    def __init__(self, json_response):
+        pass
